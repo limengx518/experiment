8c8cc4641577f563953b273eb5cdaa66d6b84516
limengxue <2304768518@qq.com>
2022-05-25 16:19:13 +0800

    内容修改

--- hello.c
+++ hello.c
@@ -7,7 +7,8 @@ int main(void)
 	//我修改了版本三
 	printf("hello moon");
 	printf("My name is ShuZhen");
-	printf("my name is liangzhuzhen")
+	printf("my name is liangzhuzhen");
+	printf("hello hello\n");
 
 	
 	return 1;
